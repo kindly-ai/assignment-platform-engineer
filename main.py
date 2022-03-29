b4a74842d3043550d0525a2cce3300a8879133fe
Mathias Ose <mathias.ose@gmail.com>
2022-03-29 13:17:18 +0200

    🚧 Get bot replies from DB

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from datetime import datetime
 from enum import Enum
 from uuid import uuid4
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlalchemy import create_engine, text
 
 app = FastAPI()
+engine = create_engine("sqlite:///silly_bot.sqlite", echo=True, future=True)
+
+
+def find_greeting() -> str:
+    with engine.connect() as conn:
+        query = "select bot_replies from greetings order by random() limit 1"
+        result = conn.execute(text(query))
+        return result.all()[0][0]
+
+
+def find_fallback() -> str:
+    with engine.connect() as conn:
+        query = "select bot_replies from fallbacks order by random() limit 1"
+        result = conn.execute(text(query))
+        return result.all()[0][0]
+
+
+def find_reply(prompt: str) -> str:
+    with engine.connect() as conn:
+        query = "select bot_replies from dialogues where user_says LIKE :prompt order by random() limit 1"
+        result = conn.execute(text(query), {"prompt": prompt.lower().strip()})
+        try:
+            return result.all()[0][0]
+        except IndexError:
+            return find_fallback()
 
 
 class Sender(str, Enum):
@@ -20,15 +45,15 @@ class Message(BaseModel):
 
 @app.post("/start_chat")
 async def start_chat():
-    return {"chat_id": uuid4(), "reply": "Greetings human!"}
+    chat_id = uuid4()
+    reply = find_greeting()
+    return {"chat_id": chat_id, "reply": reply}
 
 
 @app.post("/message")
-async def message(message: Message):
-    if message.text.lower().strip() == "tell me a joke":
-        return {"reply": "The cat with a tie."}
-    else:
-        return {"reply": "Sorry, I didn't understand that."}
+async def message(body: Message):
+    reply = find_reply(body.text)
+    return {"reply": reply}
 
 
 @app.get("/transcript/{chat_id}")
